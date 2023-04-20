faa75692ee4e15ff39a32e65cb883560cb6e5cf4
kevisam <kevin.sam@vub.be>
2023-04-20 17:39:03 +0200

    Added two sided slider

--- code/main.py
+++ code/main.py
@@ -6,7 +6,6 @@ import utils.visual_functions as visual
 import utils.helper_functions as helper
 
 
-
 ###############################
 # === Define page objects === #
 ###############################
@@ -18,8 +17,8 @@ fig, ax = visual.createPitch(pitch_width, pitch_height, "green")
 
 # Define available colors (available = 1 ; not available = 0)
 colors = {
-    'Simple pass': 'blue',
-    'High pass': 'red',
+    "Simple pass": "blue",
+    "High pass": "red",
 }
 
 
@@ -39,56 +38,57 @@ match = helper.get_match(match_id)
 events = match["subEventName"].unique().tolist()
 selected_events = st.sidebar.multiselect("Select an event type:", events)
 
-# Drop-down menu to select the time window.
-time_window = st.sidebar.number_input("Select time window (minutes): ", step=1, value=1)
-
-
 
 #######################
 # === Render page === #
 #######################
 
 # Render title
-st.title('Football Game Statistics Visualized')
+st.title("Football Game Statistics Visualized")
 
 # Render introduction
 st.markdown(
     """
     This app allows for the visualization of different actions and events that occurred during the football matches in 2016, 2017, and 2018. The visualizations can be controlled using a time slider.
     """
-    )
+)
 
 # Render header
-st.write("");st.write("")
-st.subheader('Event visualizer')
+st.write("")
+st.write("")
+st.subheader("Event visualizer")
 
 
 # Render slider
 slider_label = "The visualization below shows the locations of events for a chosen event type, performed during a particular match, chosen team(s). Events can be filtered by team or even by player. The time window (in minutes) can be adjusted in the sidebar. The starting time (in minutes) can be set using the slider below."
-game_time = st.slider(label=slider_label, 
-                      min_value=0,
-                      max_value=120,
-                      step=1)
+# game_time = st.slider(label=slider_label,
+#                       min_value=0,
+#                       max_value=120,
+
+#                       step=1)
+
+game_time = st.slider("Select a time period", 0, 120, (0, 45), step=1)
 
 # Define arrows on pitch
 if "Simple pass" in selected_events:
-    ax = event.simple_pass_render(pitch_height=pitch_height,
-                                  pitch_width=pitch_width,
-                                  match=match, 
-                                  game_time=game_time, 
-                                  time_window=time_window,
-                                  color=colors["Simple pass"],
-                                  ax=ax)
+    ax = event.simple_pass_render(
+        pitch_height=pitch_height,
+        pitch_width=pitch_width,
+        match=match,
+        game_time=game_time,
+        color=colors["Simple pass"],
+        ax=ax,
+    )
 
 if "High pass" in selected_events:
-    ax = event.high_pass_render(pitch_height=pitch_height,
-                                pitch_width=pitch_width,
-                                match=match, 
-                                game_time=game_time, 
-                                time_window=time_window,
-                                color=colors["High pass"],
-                                ax=ax)
-
+    ax = event.high_pass_render(
+        pitch_height=pitch_height,
+        pitch_width=pitch_width,
+        match=match,
+        game_time=game_time,
+        color=colors["High pass"],
+        ax=ax,
+    )
 
 
 #######################
@@ -100,12 +100,12 @@ fig.set_size_inches(15, 10)
 st.pyplot(fig)
 
 
-
 #######################
 # === For testing === #
 #######################
 
 # Render raw data
-st.write("");st.write("")
-st.subheader('Raw data')
+st.write("")
+st.write("")
+st.subheader("Raw data")
 st.write(helper.get_match(match_id))
--- code/utils/event_functions.py
+++ code/utils/event_functions.py
@@ -1,30 +1,56 @@
-def simple_pass_render(pitch_height, pitch_width, match, game_time, time_window, color, ax):
-    """ For a given match ID and game time, returns plot elements that visualize the simple passes in the form of arrows """
+import streamlit as st
+
+
+def simple_pass_render(pitch_height, pitch_width, match, game_time, color, ax):
+    """For a given match ID and game time, returns plot elements that visualize the simple passes in the form of arrows"""
+
+    st.write(game_time)
+
     # get match data
     data = match[match["subEventName"] == "Simple pass"]
-    data = data[data["eventSec"] >= game_time*60]
-    data = data[data["eventSec"] <= game_time*60 + time_window*60]
+    data = data[data["eventSec"] >= game_time[0] * 60]
+    data = data[data["eventSec"] <= game_time[1] * 60]
 
     # create plot elements
     for idx, event in data.iterrows():
-        start_point = (event["pos_orig_x"]/100*pitch_width, event["pos_orig_y"]/100*pitch_height)
-        end_point = (event["pos_dest_x"]/100*pitch_width, event["pos_dest_y"]/100*pitch_height)
-        ax.annotate('', xy=end_point, xytext=start_point,
-            arrowprops=dict(facecolor=color, edgecolor=color, arrowstyle='->'))
+        start_point = (
+            event["pos_orig_x"] / 100 * pitch_width,
+            event["pos_orig_y"] / 100 * pitch_height,
+        )
+        end_point = (
+            event["pos_dest_x"] / 100 * pitch_width,
+            event["pos_dest_y"] / 100 * pitch_height,
+        )
+        ax.annotate(
+            "",
+            xy=end_point,
+            xytext=start_point,
+            arrowprops=dict(facecolor=color, edgecolor=color, arrowstyle="->"),
+        )
     return ax
 
 
-def high_pass_render(pitch_height, pitch_width, match, game_time, time_window, color, ax):
-    """ For a given match ID and game time, returns plot elements that visualize the simple passes in the form of arrows """
+def high_pass_render(pitch_height, pitch_width, match, game_time, color, ax):
+    """For a given match ID and game time, returns plot elements that visualize the simple passes in the form of arrows"""
     # get match data
     data = match[match["subEventName"] == "High pass"]
-    data = data[data["eventSec"] >= game_time*60]
-    data = data[data["eventSec"] <= game_time*60 + time_window*60]
+    data = data[data["eventSec"] >= game_time[0] * 60]
+    data = data[data["eventSec"] <= game_time[1] * 60]
 
     # create plot elements
     for idx, event in data.iterrows():
-        start_point = (event["pos_orig_x"]/100*pitch_width, event["pos_orig_y"]/100*pitch_height)
-        end_point = (event["pos_dest_x"]/100*pitch_width, event["pos_dest_y"]/100*pitch_height)
-        ax.annotate('', xy=end_point, xytext=start_point,
-            arrowprops=dict(facecolor=color, edgecolor=color, arrowstyle='->'))
+        start_point = (
+            event["pos_orig_x"] / 100 * pitch_width,
+            event["pos_orig_y"] / 100 * pitch_height,
+        )
+        end_point = (
+            event["pos_dest_x"] / 100 * pitch_width,
+            event["pos_dest_y"] / 100 * pitch_height,
+        )
+        ax.annotate(
+            "",
+            xy=end_point,
+            xytext=start_point,
+            arrowprops=dict(facecolor=color, edgecolor=color, arrowstyle="->"),
+        )
     return ax
