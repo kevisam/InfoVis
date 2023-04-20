a3f8e682574594eb61ceaf543c782e60d34eef0d
Bram Silue <bramsilue@Brams-MacBook-Pro.local>
2023-04-20 02:44:39 +0200

    minor corrections

--- code/utils/helper_functions.py
+++ code/utils/helper_functions.py
@@ -5,14 +5,14 @@ from matplotlib import pyplot as plt
 def load_data(nrows):
     """ Loads and returns the football data """
 
-    data = pd.read_csv("dataset/events_World_Cup.csv", nrows=nrows)
+    data = pd.read_csv("./code/dataset/events_World_Cup.csv", nrows=nrows)
     return data
 
 
 def get_match(matchId):
     """ For a given match ID, returns the match data in a pandas dataframe """
 
-    data = pd.read_csv("dataset/events_World_Cup.csv")
+    data = pd.read_csv("./code/dataset/events_World_Cup.csv")
     matches_with_id = data[data["matchId"] == matchId]
     return matches_with_id
 
@@ -20,7 +20,7 @@ def get_match(matchId):
 def get_team_side(matchId, teamId):
     """ For a given match ID, and team ID returns the side on which the team plays on the pitch """
     
-    data = pd.read_csv("dataset/events_World_Cup.csv")
+    data = pd.read_csv("./code/dataset/events_World_Cup.csv")
     match = get_match(matchId)
     unique_team_ids = match["teamId"].unique()
 
