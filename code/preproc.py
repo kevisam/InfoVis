967483049a4b55eebb2f495cfe4a9cc0280da7c8
kevisam <kevin.sam@vub.be>
2023-04-21 11:41:40 +0200

    Added actions to preprocessing

--- code/preproc.py
+++ code/preproc.py
@@ -2,19 +2,21 @@ import pandas as pd
 
 # get events and remove unnecessary columns
 events = pd.read_csv("./code/dataset/data_raw/events_World_Cup.csv")
-events.drop('eventId', axis=1, inplace=True)
-events.drop('eventName', axis=1, inplace=True)
-events.drop('subEventId', axis=1, inplace=True)
-events.drop('tags', axis=1, inplace=True)
-events.drop('tagsList', axis=1, inplace=True)
-events.drop('positions', axis=1, inplace=True)
+events.drop("eventId", axis=1, inplace=True)
+events.drop("eventName", axis=1, inplace=True)
+events.drop("subEventId", axis=1, inplace=True)
+events.drop("tags", axis=1, inplace=True)
+events.drop("tagsList", axis=1, inplace=True)
+events.drop("positions", axis=1, inplace=True)
 
 # get match labels
 matches = pd.read_csv("./code/dataset/data_raw/matches_World_Cup.csv")
 matches = matches[["dateutc", "wyId", "label", "teamsData"]]
-matches['match_name'] = matches.apply(lambda x: f"{x['label']} ({x['dateutc']})", axis=1)
-matches.drop('dateutc', axis=1, inplace=True)
-matches.drop('label', axis=1, inplace=True)
+matches["match_name"] = matches.apply(
+    lambda x: f"{x['label']} ({x['dateutc']})", axis=1
+)
+matches.drop("dateutc", axis=1, inplace=True)
+matches.drop("label", axis=1, inplace=True)
 
 # get teams
 teams = pd.read_csv("./code/dataset/data_raw/teams.csv")
@@ -22,17 +24,39 @@ teams = teams[["wyId", "officialName"]]
 
 # get players
 players = pd.read_csv("./code/dataset/data_raw/players.csv")
-players.drop('birthArea', axis=1, inplace=True)
-players.drop('firstName', axis=1, inplace=True)
-players.drop('middleName', axis=1, inplace=True)
-players.drop('lastName', axis=1, inplace=True)
-players.drop('currentNationalTeamId', axis=1, inplace=True)
+players.drop("birthArea", axis=1, inplace=True)
+players.drop("firstName", axis=1, inplace=True)
+players.drop("middleName", axis=1, inplace=True)
+players.drop("lastName", axis=1, inplace=True)
+players.drop("currentNationalTeamId", axis=1, inplace=True)
 
 # print
 print(events)
 print(matches)
 print(teams)
 print(players)
+
+# Add the action number
+events = events[
+    [
+        "matchId",
+        "subEventName",
+        "eventSec",
+        "playerId",
+        "pos_orig_x",
+        "pos_orig_y",
+        "pos_dest_x",
+        "pos_dest_y",
+        "teamId",
+    ]
+]
+events["action"] = 0
+events["action"] = events.groupby("matchId", group_keys=False)["teamId"].apply(
+    lambda x: (x != x.shift()).cumsum()
+)
+# format action column as required
+events["action"] = "action" + events["action"].astype(str)
+
 
 # store data
 events.to_csv("./code/dataset/data_clean/clean_events_data.csv", index=False)
--- code/utils/helper_functions.py
+++ code/utils/helper_functions.py
@@ -16,44 +16,50 @@ players_path = "./code/dataset/data_clean/clean_players_data.csv"
 # === Functions ===#
 ####################
 
+
 def load_all_events():
-    """ Loads and returns the football events data """
+    """Loads and returns the football events data"""
 
     data = pd.read_csv(events_path)
     return data
 
+
 def load_all_matches():
-    """ Loads and returns the football matches data """
-    
+    """Loads and returns the football matches data"""
+
     data = pd.read_csv(matches_path)
     return data
 
+
 def load_all_teams():
-    """ Loads and returns the football matches data """
-    
+    """Loads and returns the football matches data"""
+
     data = pd.read_csv(teams_path)
     return data
 
+
 def load_all_players():
-    """ Loads and returns the football matches data """
-    
+    """Loads and returns the football matches data"""
+
     data = pd.read_csv(players_path)
     return data
 
+
 def get_match_events(matchId, all_events_data, teamId, players):
-    """ For a given match ID and all events data, returns the match events data in a pandas dataframe """
+    """For a given match ID and all events data, returns the match events data in a pandas dataframe"""
 
     filtered_data = all_events_data[all_events_data["matchId"] == matchId]
-    filtered_data = filtered_data[filtered_data["teamId"] ==  teamId]
+    filtered_data = filtered_data[filtered_data["teamId"] == teamId]
     if players == "all":
         return filtered_data
     else:
         filtered_data = filtered_data[filtered_data["playerId"].isin(players)]
         return filtered_data
 
+
 def get_team_side(matchId, teamId, events_data):
-    """ For a given match ID, and team ID returns the side on which the team plays on the pitch """
-    
+    """For a given match ID, and team ID returns the side on which the team plays on the pitch"""
+
     data = pd.read_csv(events_path)
     match = get_match_events(matchId, events_data)
     unique_team_ids = match["teamId"].unique()
